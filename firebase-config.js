93dcf6aeecf6c09cf91b55d2a5bab20c13524b1b
Angel-Malik-3008 <angel030btcse26@igdtuw.ac.in>
2026-09-15 19:27:38 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCZZg0kziNBSV5UKqbOWNsC7_CAb55KP1Y",
+  authDomain: "campus-buddy-ccbc3.firebaseapp.com",
+  projectId: "campus-buddy-ccbc3",
+  storageBucket: "campus-buddy-ccbc3.firebasestorage.app",
+  messagingSenderId: "513141882285",
+  appId: "1:513141882285:web:ddea7000c4f3e9e5503524",
+  measurementId: "G-WENLCZ1ZH"
+};
+
+export { firebaseConfig };
