1cea73067b8334fc024800a0fa8d5b0be0b2ec76
Angel-Malik-3008 <angel030btcse26@igdtuw.ac.in>
2026-09-15 20:15:04 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCZZg0kziNBSV5UKqbOWNsC7_CAb55KP1Y",
+  apiKey: "AIzaSyCZZgOkziNBSV5UKqbOWNsC7_CAb55KP1Y",
   authDomain: "campus-buddy-ccbc3.firebaseapp.com",
   projectId: "campus-buddy-ccbc3",
   storageBucket: "campus-buddy-ccbc3.firebasestorage.app",
   messagingSenderId: "513141882285",
   appId: "1:513141882285:web:ddea7000c4f3e9e5503524",
-  measurementId: "G-WENLCZ1ZH"
+  measurementId: "G-WENCLZK1ZH"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export { firebaseConfig };
